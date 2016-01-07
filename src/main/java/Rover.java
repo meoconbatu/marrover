61f858fb7f29fa67014b49d265573690d30f375c
Thanh Mi Nguyen Thanh <thanhmi@Thanhs-MacBook-Air.local>
2016-01-07 22:10:09 +0700

    rover add property instruction

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,11 +1,17 @@
 public class Rover {
     public static Plateau plateau;
-    private CoOrdinate currentCoOrinate;
+    private CoOrdinate currentCoOrdinate;
     private String orientation;
+    private String instruction;
 
     public Rover(CoOrdinate startingOrdinate, String orientation) {
-        currentCoOrinate = startingOrdinate;
+        currentCoOrdinate = startingOrdinate;
         this.orientation = orientation;
+        instruction = null;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
     }
 
     public void executeCommand(char m) {
@@ -20,49 +26,60 @@ public class Rover {
         }
     }
     private void turnLeft(){
-        if (orientation == "S")
+        if (orientation.equals("S"))
             orientation = "E";
-        else if (orientation == "W")
+        else if (orientation.equals("W"))
             orientation = "S";
-        else if (orientation == "N")
+        else if (orientation.equals("N"))
             orientation = "W";
-        else if (orientation == "E")
+        else if (orientation.equals("E"))
             orientation = "N";
     }
     private void turnRight(){
-        if (orientation == "S")
+        if (orientation.equals("S"))
             orientation = "W";
-        else if (orientation == "W")
+        else if (orientation.equals("W"))
             orientation = "N";
-        else if (orientation == "N")
+        else if (orientation.equals("N"))
             orientation = "E";
-        else if (orientation == "E")
+        else if (orientation.equals("E"))
             orientation = "S";
     }
     private void move()
     {
-        if (orientation == "E"){
-            if (plateau.getUpperRight().getX() != currentCoOrinate.getX())
-                currentCoOrinate = new CoOrdinate(currentCoOrinate.getX()+1, currentCoOrinate.getY());
+        if (orientation.equals("E")){
+            if (plateau.getUpperRight().getX() != currentCoOrdinate.getX())
+                currentCoOrdinate = new CoOrdinate(currentCoOrdinate.getX()+1, currentCoOrdinate.getY());
         }
-        else if (orientation == "W"){
-            if (plateau.getLowerLeft().getX() != currentCoOrinate.getX())
-                currentCoOrinate = new CoOrdinate(currentCoOrinate.getX()-1, currentCoOrinate.getY());
+        else if (orientation.equals("W")){
+            if (plateau.getLowerLeft().getX() != currentCoOrdinate.getX())
+                currentCoOrdinate = new CoOrdinate(currentCoOrdinate.getX()-1, currentCoOrdinate.getY());
         }
-        else if (orientation == "N"){
-            if (plateau.getUpperRight().getY() != currentCoOrinate.getY())
-                currentCoOrinate = new CoOrdinate(currentCoOrinate.getX(), currentCoOrinate.getY()+1);
+        else if (orientation.equals("N")){
+            if (plateau.getUpperRight().getY() != currentCoOrdinate.getY())
+                currentCoOrdinate = new CoOrdinate(currentCoOrdinate.getX(), currentCoOrdinate.getY()+1);
         }
-        else if (orientation == "S"){
-            if (plateau.getLowerLeft().getY() != currentCoOrinate.getY())
-                currentCoOrinate = new CoOrdinate(currentCoOrinate.getX(), currentCoOrinate.getY()-1);
+        else if (orientation.equals("S")){
+            if (plateau.getLowerLeft().getY() != currentCoOrdinate.getY())
+                currentCoOrdinate = new CoOrdinate(currentCoOrdinate.getX(), currentCoOrdinate.getY()-1);
         }
     }
-    public CoOrdinate getCurrentCoOrinate() {
-        return currentCoOrinate;
+    public void executeInstruction() {
+        for (int i = 0; i < instruction.length(); i++) {
+            executeCommand(instruction.charAt(i));
+        }
+    }
+    public CoOrdinate getCurrentCoOrdinate() {
+        return currentCoOrdinate;
     }
 
     public String getCurrentOrientation() {
         return orientation;
     }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -8,7 +8,7 @@ public class RoverTest {
 
         rover.executeCommand('M');
 
-        assertEqualsCoOrdinate(new CoOrdinate(4, 5), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(4, 5), rover.getCurrentCoOrdinate());
     }
 
     @Test
@@ -17,7 +17,7 @@ public class RoverTest {
 
         rover.executeCommand('M');
 
-        assertEqualsCoOrdinate(new CoOrdinate(4, 5), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(4, 5), rover.getCurrentCoOrdinate());
     }
 
     @Test
@@ -26,7 +26,7 @@ public class RoverTest {
 
         rover.executeCommand('M');
 
-        assertEqualsCoOrdinate(new CoOrdinate(5, 5), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(5, 5), rover.getCurrentCoOrdinate());
     }
 
     @Test
@@ -35,32 +35,32 @@ public class RoverTest {
 
         rover.executeCommand('M');
 
-        assertEqualsCoOrdinate(new CoOrdinate(5, 4), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(5, 4), rover.getCurrentCoOrdinate());
     }
 
     @Test
     public void cantMoveWhenTouchNorthBorder() {
         Rover rover = createRover(4, 5, "N");
         rover.executeCommand('M');
-        assertEqualsCoOrdinate(new CoOrdinate(4, 5), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(4, 5), rover.getCurrentCoOrdinate());
     }
     @Test
     public void cantMoveWhenTouchWestBorder() {
         Rover rover = createRover(0, 3, "W");
         rover.executeCommand('M');
-        assertEqualsCoOrdinate(new CoOrdinate(0, 3), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(0, 3), rover.getCurrentCoOrdinate());
     }
     @Test
     public void cantMoveWhenTouchEastBorder() {
         Rover rover = createRover(5, 3, "E");
         rover.executeCommand('M');
-        assertEqualsCoOrdinate(new CoOrdinate(5, 3), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(5, 3), rover.getCurrentCoOrdinate());
     }
     @Test
     public void cantMoveWhenTouchSouthBorder() {
         Rover rover = createRover(2, 0, "S");
         rover.executeCommand('M');
-        assertEqualsCoOrdinate(new CoOrdinate(2, 0), rover.getCurrentCoOrinate());
+        assertEqualsCoOrdinate(new CoOrdinate(2, 0), rover.getCurrentCoOrdinate());
     }
     @Test
     public void canTurnLeftFromSouth() {
@@ -110,6 +110,14 @@ public class RoverTest {
         rover.executeCommand('R');
         Assert.assertEquals("N", rover.getCurrentOrientation());
     }
+    @Test
+    public void canfollowInstruction() {
+        Rover rover = createRover(1, 2, "N");
+        rover.setInstruction("LMLMLMLMM");
+        rover.executeInstruction();
+        assertEqualsRover(new Rover(new CoOrdinate(1,3), "N"), rover);
+    }
+
     private Rover createRover(int x, int y, String orientation) {
         CoOrdinate startingOrdinate = new CoOrdinate(x, y);
         Rover.plateau = new Plateau(new CoOrdinate(0,0), new CoOrdinate(5,5));
@@ -119,5 +127,9 @@ public class RoverTest {
     private void assertEqualsCoOrdinate(CoOrdinate expected, CoOrdinate actual) {
         Assert.assertEquals(expected.getX(), actual.getX());
         Assert.assertEquals(expected.getY(), actual.getY());
+    }
+    private void assertEqualsRover(Rover expected, Rover actual) {
+        Assert.assertEquals(expected.getCurrentOrientation(), actual.getCurrentOrientation());
+        assertEqualsCoOrdinate(expected.getCurrentCoOrdinate(), actual.getCurrentCoOrdinate());
     }
 }
