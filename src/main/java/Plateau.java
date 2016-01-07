df06c2bbcbc710dd1c564692e47e3e3c8bd50488
Thanh Mi Nguyen Thanh <thanhmi@Thanhs-MacBook-Air.local>
2016-01-07 23:06:35 +0700

    add main function

--- src/main/java/Plateau.java
+++ src/main/java/Plateau.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Plateau {
     private final CoOrdinate lowerLeft;
     private final CoOrdinate upperRight;
@@ -11,5 +13,18 @@ public class Plateau {
     }
     public CoOrdinate getLowerLeft() {
         return lowerLeft;
+    }
+    public static void main(String [] args)
+    {
+        String result = "";
+        FileHelper fileHelper = new FileHelper("inputMarRover.txt");
+        List<Rover> iRovers = fileHelper.readRoverInfos();
+        for (int i = 0; i < iRovers.size(); i++) {
+            iRovers.get(i).executeInstruction();
+            result += iRovers.get(i).getCurrentCoOrdinate().getX() + " "
+                    + iRovers.get(i).getCurrentCoOrdinate().getY() + " "
+                    + iRovers.get(i).getCurrentOrientation() + "\n";
+        }
+        fileHelper.writeResult(result);
     }
 }
